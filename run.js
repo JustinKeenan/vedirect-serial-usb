02280e711510e5a459f6561f1cd36ccb60beb65b
JustinKeenan <91327115+JustinKeenan@users.noreply.github.com>
2021-10-17 11:45:54 -0400

    Update run.js
    Needs to update UDP port to be device specific

--- run.js
+++ run.js
@@ -3,6 +3,7 @@ const app = {
   handleMessage: (id, delta) => console.log(JSON.stringify(delta, null, 2))
 }
 
+//UDP port port should be device specified
 const plugin = require('./')(app)
 plugin.start({
   udpPort: 7878,
